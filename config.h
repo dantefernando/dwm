fd91927f289fe09d98b01bbca8899d279e4405fe
dantefernando@github.com <coolguymctmg@gmail.com>
2021-09-06 20:02:35 +0200

    Added brightness controls

--- config.h
+++ config.h
@@ -191,7 +191,7 @@ static Key keys[] = {
 
 	/* { MODKEY,			    XK_p,			spawn,		SHCMD("mpc toggle") }, */
 	/* { MODKEY|ShiftMask,		XK_p,			spawn,		SHCMD("mpc pause ; pauseallmpv") }, */
-	/* { MODKEY|ShiftMask,		XK_p,			spawn,		SHCMD("poweroff") }, */
+	{ MODKEY|ShiftMask,		XK_p,			spawn,		SHCMD("firefox --private-window") },
 	{ MODKEY,			    XK_bracketleft,		spawn,		SHCMD("mpc seek -10") },
 	{ MODKEY|ShiftMask,		XK_bracketleft,		spawn,		SHCMD("mpc seek -60") },
 	{ MODKEY,			    XK_bracketright,	spawn,		SHCMD("mpc seek +10") },
@@ -264,8 +264,10 @@ static Key keys[] = {
 	{ MODKEY,			XK_F10,		spawn,		SHCMD("~/.local/bin/dmenuumount") },
 	/* { MODKEY,			XK_F11,		spawn,		SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") }, */
 	{ MODKEY,			XK_F11,		spawn,		SHCMD("xbacklight -dec 15; pkill -RTMIN+9 dwmblocks") },
+	{ MODKEY|ShiftMask,			XK_F11,		spawn,		SHCMD("xbacklight -set 0.01; pkill -RTMIN+9 dwmblocks") },
 	/* { MODKEY,			XK_F12,		spawn,		SHCMD("remaps & notify-send \\\"⌨️ Keyboard remapping...\\\" \\\"Re-running keyboard defaults for any newly plugged-in keyboards.\\\"") }, */
 	{ MODKEY,			XK_F12,		spawn,		SHCMD("xbacklight -inc 15; pkill -RTMIN+9 dwmblocks") },
+	{ MODKEY|ShiftMask,			XK_F12,		spawn,		SHCMD("xbacklight -set 100; pkill -RTMIN+9 dwmblocks") },
 	{ MODKEY,			XK_space,	zoom,		{0} },
 	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
 
